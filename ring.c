a7f578a1ff1a15a674c1becdf237f29ed213b02c
DonnellWashington <wasnelly4@gmail.com>
2025-02-27 12:34:55 -0500

    Ok ok for real

--- ring.c
+++ ring.c
@@ -1,26 +1,9 @@
-#include<sys/socket.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<string.h>
-#include<stdbool.h>
-#include<stdio.h>
 
-void dieWithUserMessage(const char *msg, const char *detail);             // HANDLING ERROR WITH USER MESSAGE
-
-void DieWithSystemMessage(const char *msg, const char * detail);            // MIGHT NEED TO HANDLING ERROR WITH SYS MESSAGE
-
-//void PrintSocketAddress(const struct sockAddr *address, FILE *stream);      //WE PROB WONT NEED THIS
-
-bool SockAddrsEqual(const struct sockaddr *addr1, const st);                //PROB WONT NEED THIS EITHER
-
-int setupTCPServerSocket(const char *service);                              // CREATING THE SERVER SOCKET PORT NUMBER AS PARAMETER
-
-int accpetTCPConnection(int clntSock);
-
-int setupTCPClientSocket(const char *server, const char *service);
 
 
 int main(int argc, char *argv[]){
+
+
 
     return 0;
 
